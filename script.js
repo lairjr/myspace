d9421e96a98881061fef08eea2c6745feded4a95
Lair Junior <lairjr@outlook.com>
2024-08-07 17:50:22 +0000

    Update website

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'end' });
 }
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+};
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+}, observerOptions);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const fadeElems = document.querySelectorAll('.fade');
+    const lineElems = document.querySelectorAll('.line');
+
+    fadeElems.forEach(element => {
+        observer.observe(element);
+    });
+
+    lineElems.forEach(element => {
+        observer.observe(element);
+    });
+});
